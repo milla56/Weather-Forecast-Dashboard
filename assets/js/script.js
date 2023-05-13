e6a04401939a8ef0fcd430f3b5eda78336fd382f
milla56 <lubomila14@gmail.com>
2023-05-13 15:11:30 +0100

    setting api and ajax to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,25 @@ weatherSearch = $(".weather-search");
 today = $("$today");
 forecast = $("$forecast");
 searchInput = $("#search-input");
+var city = "";
 
 //API KEY
 var APIKey = "758974132e2e4da4f5697230761019a3"; 
+
+//getting the data from from server
+function currentWeather(city){
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method: "GET",
+    }).then(function(response){
+        console.log(response);
+
+
+
+    })
+}
 
 
 
