5420666aae6809f05b3298ce56b27b2b4f8f5f7b
milla56 <lubomila14@gmail.com>
2023-05-13 14:47:17 +0100

    setting variables to js

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+function initPage() {
+
+//Variables
+
+searchButton = $("#search-button");
+cityList = $(".list-group");
+weatherSearch = $(".weather-search");
+today = $("$today");
+forecast = $("$forecast");
+searchInput = $("#search-input");
+
+//API KEY
+var APIKey = "758974132e2e4da4f5697230761019a3"; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+ 
+
+
